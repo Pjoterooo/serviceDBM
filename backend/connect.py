d2fb699d559aec9332100b54c617ccd1ed3d083a
Piotr <piotrsala02@gmail.com>
2024-03-14 14:07:47 +0100

    addToConnectEndPointAndUpdatePagetsx

--- backend/connect.py
+++ backend/connect.py
@@ -6,11 +6,12 @@ from config import DB_USER, DB_PASSWORD, DB_DSN
 app = Flask(__name__)
 CORS(app)
 
+user = DB_USER
+password = DB_PASSWORD
+dsn = DB_DSN
+
 @app.route('/samochody', methods=['GET'])
 def get_samochody():
-    user = DB_USER
-    password = DB_PASSWORD
-    dsn = DB_DSN
 
     try:
         connection = oracledb.connect(user=user, password=password, dsn=dsn)
@@ -26,6 +27,24 @@ def get_samochody():
         return jsonify(samochody)
     except oracledb.DatabaseError as error:
         return jsonify({'error': str(error)})
+
+@app.route('/dodajsamochody', methods=['POST'])
+def add_samochody():
+    try:
+        data = request.json
+        connection = oracledb.connect(user=user, password=password, dsn=dsn)
+        cursor = connection.cursor()
+        sql = "INSERT INTO samochody2 (vin, nr_rejestracyjny, marka, typ, cena, uwagi) VALUES(:vin, :nr_rejestracyjny, :marka, :typ, :cena, :uwagi)"
+        cursor.execute(sql, {'vin': data['vin'], 'nr_rejestracyjny': data['nr_rejestracyjny'], 'marka': data['marka'], 'typ': data['typ'], 'cena': data['cena'], 'uwagi': data['uwagi']})
+        connection.commit()
+
+        cursor.close()
+        connection.close()
+
+        return jsonify({'succes' : True , 'message': 'Samochod dodany'})
+    
+    except (oracledb.DatabaseError, KeyError) as error:
+        return jsonify({'success': False, 'error': str(error)}), 400
 
 if __name__ == '__main__':
     app.run(debug=True, host = '127.0.0.1', port = 5000)
--- my-app/src/app/testComponents/page.tsx
+++ my-app/src/app/testComponents/page.tsx
@@ -1,9 +1,38 @@
 "use client";
 import "../tailwind.css";
-import ActiveUser from "../components/activeUser"
+import ActiveUser from "../components/activeUser";
+import { useState, useEffect } from "react";
 
 export default function Main() {
-    return (
-        <ActiveUser></ActiveUser>
-    )
+  let [liczba, setLiczba] = useState(0);
+  console.log(liczba);
+
+  const [windowSize, setWindowSize] = useState({width: window.innerWidth,height: window.innerHeight,});
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <div>
+      <ActiveUser></ActiveUser>
+      <button onClick={() => setLiczba(liczba + 1)}>+</button>
+      <p>Liczba: {liczba}</p>
+      <div>
+        <p>Szerokość okna: {windowSize.width}px</p>
+        <p>Wysokość okna: {windowSize.height}px</p>
+      </div>
+    </div>
+  );
 }
